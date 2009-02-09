4ae9e0ea018349c43b4e4a02779a1b7393ee3acc
Bradley Dean <bjdean@bjdean.id.au>
2009-02-09 23:49:10 +0000

    Import of old site from http://cam.pm.org/ and partially updated site from http://riotprrrl.com/.

---
+++ old-site/list/admin/cam.pm
@@ -0,0 +1,46 @@
+
+<html> <head>
+  <title>Cam.pm Administrator Authentication</title>
+</head> <body bgcolor="#ffffff"> <FORM METHOD=POST
+ACTION="/list/admin/cam.pm">
+
+  <TABLE WIDTH="100%" BORDER="0" CELLSPACING="4" CELLPADDING="5">
+    <TR>
+      <TD COLSPAN="2" WIDTH="100%" BGCOLOR="#99CCFF" ALIGN="CENTER">
+	<B><FONT COLOR="#000000" SIZE="+1">Cam.pm Administrator
+	    Authentication</FONT></B>
+      </TD>
+    </TR>
+    <tr>
+      <TD><div ALIGN="Right">List Administrator Password:</div></TD>
+      <TD><INPUT TYPE="password" NAME="adminpw" SIZE="30"></TD>
+    </tr>
+    <tr>
+      <td colspan=2 align=middle><INPUT type="SUBMIT"
+                                        name="admlogin"
+					value="Let me in...">
+      </td>
+    </tr>
+  </TABLE>
+      <p><strong><em>Important:</em></strong> From this point on, you
+      must have cookies enabled in your browser, otherwise no
+      administrative changes will take effect.
+
+      <p>Session cookies are used in Mailman's
+      administrative interface so that you don't need to
+      re-authenticate with every administrative operation.  This
+      cookie will expire automatically when you exit your browser, or
+      you can explicitly expire the cookie by hitting the
+      <em>Logout</em> link under <em>Other Administrative
+      Activities</em> (which you'll see once you successfully log in).
+</FORM> </body> </html>
+
+<hr><address><a href="http://cam.pm.org/list/listinfo/cam.pm">Cam.pm</a> list run by <a href="mailto:cam.pm-owner@cam.pm.org">mbm at colondot.net, lucifer at slimy.greenend.org.uk</a><br><a href="http://cam.pm.org/list/admin/cam.pm">Cam.pm administrative interface</a> (requires authorization)<br><a href="http://cam.pm.org/mailman/listinfo">Overview of all cam.pm.org mailing lists</a><p>
+<table WIDTH="100%" BORDER="0">
+  <tr>
+    <td><img src="/icons/mailman.jpg" alt="Delivered by Mailman" border=0><br>version 2.1.6</td>
+    <td><img src="/icons/PythonPowered.png" alt="Python Powered" border=0></td>
+    <td><img src="/icons/powerlogo.gif" alt="Powered by FreeBSD" border=0></td>
+  </tr>
+</table>
+</address>
